7d8776a292384e79b371650652d703173d60ca33
Ruta Bendoraityte <90304014+rutaben@users.noreply.github.com>
2022-05-27 15:28:24 +0300

    feat: created a primary routing for login, register and movies list pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import theme from './styles/theme';
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
+import LoginPage from './pages/login-page';
+import RegisterPage from './pages/register-page';
 import MoviesPage from './pages/movies-page';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <MoviesPage />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/favorite-movies" element={<MoviesPage />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 };
