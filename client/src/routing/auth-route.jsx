14832bee52ecae507924b4877d508c9d5e6e39fb
Ruta Bendoraityte <90304014+rutaben@users.noreply.github.com>
2022-06-02 05:41:52 +0300

    feat: enabled user logout

--- client/src/routing/auth-route.jsx
+++ client/src/routing/auth-route.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import SessionService from '../services/session-service';
+// import SessionService from '../services/session-service';
 import LoginPage from '../pages/login-page';
 
 const AuthRoute = () => {
-  const authState = SessionService.get('auth');
+  const authState = JSON.parse(localStorage.getItem('auth'));
   console.log(authState);
 
-  if (!authState.loggedIn) {
+  if (!authState || authState.loggedIn === false) {
     return <LoginPage />;
   } else {
     return <Navigate to="/favorite-movies" replace />;
