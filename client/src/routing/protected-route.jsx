186d5e7bba5f555ee0046618d99dd07f5a6a526a
Ruta Bendoraityte <90304014+rutaben@users.noreply.github.com>
2022-06-06 03:15:13 +0300

    refactor: used Local Storage Service in routes

--- client/src/routing/protected-route.jsx
+++ client/src/routing/protected-route.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import LocalStorageService from '../services/local-storage-service';
 import MoviesPage from '../pages/movies-page';
 
 const ProtectedRoute = () => {
-  const authState = JSON.parse(localStorage.getItem('auth'));
+  const authState = LocalStorageService.get('auth');
   const userFirstName = authState ? authState.user.firstName : null;
 
   if (authState && authState.loggedIn) {
